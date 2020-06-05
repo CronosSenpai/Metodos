624a811282d3d600308b5e3b38e5c0f702b85a1e
CronosSenpai <jordivs00@gmail.com>
2020-06-05 06:29:52 +0200

    Creacion

---
+++ Bolzano/Bolzano.f90
@@ -0,0 +1,25 @@
+!  Bolzano.f90 
+!
+!  FUNCTIONS:
+!  Bolzano - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Bolzano
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Bolzano
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Bolzano
+    print *, 'Hello World'
+
+    end program Bolzano
+
